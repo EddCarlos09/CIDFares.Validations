69f5e50766a76eebb9002266b800d279abaac32c
Edrey Cruz <eacr_777@hotmail.com>
2019-11-15 15:33:37 -0600

    RFC and Phone number validations added

--- CIDFares.Library.Validations/RegularExpression/Validations.cs
+++ CIDFares.Library.Validations/RegularExpression/Validations.cs
@@ -27,5 +27,41 @@ namespace CIDFares.Library.Validations
             string pattern = @"^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$";
             return Regex.IsMatch(value, pattern);
         }
+
+
+        /// <summary>
+        /// Validation will fall if the value is no a valid RFC
+        /// </summary>
+        /// <param name="value">Value to validate</param>
+        /// <returns></returns>
+        public static bool IsValidRFC(string value)
+        {
+            string pattern = @"^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$";
+            return Regex.IsMatch(value, pattern);
+        }
+
+
+        /// <summary>
+        /// Validation for ten digits for a number phone
+        /// </summary>
+        /// <param name="value">Value to validate</param>
+        /// <returns></returns>
+        public static bool IsValidPhoneNumberDigits(string value)
+        {
+            string pattern = @"^[\d]{10}$";
+            return Regex.IsMatch(value, pattern);
+        }
+
+
+        /// <summary>
+        /// Validation for number phone with () and --
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsValidPhoneNumber(string value)
+        {
+            string pattern = @"(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/";
+            return Regex.IsMatch(value, pattern);
+        }
     }
 }
